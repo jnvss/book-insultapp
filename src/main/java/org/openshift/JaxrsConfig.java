b98db905fcc9c77db422136a8d9b94893f2d093a
root <root@h50l020.suvanet.ch>
2017-09-25 10:08:01 +0200

    Postresql Database Implementation

--- src/main/java/org/openshift/JaxrsConfig.java
+++ src/main/java/org/openshift/JaxrsConfig.java
@@ -3,6 +3,6 @@ package org.openshift;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/ws")
+@ApplicationPath("/api")
 public class JaxrsConfig extends Application{
 }
